4aa62d695568d0f81a5c05d4770a22329e7b435d
Cedric Heijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-11-03 13:56:48 +0100

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className={styles.navBar}>
+      <ul className={styles.navBarList}>
+        <li>About</li>
+        <li>Discover</li>
+        <li id={styles.logo}>Viewr</li>
+        <li>Users</li>
+        <li>Account</li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
