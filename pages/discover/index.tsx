4b9c9c4b875c74bfcf810041b679f00a7d1f0045
Cedric Heijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-11-03 21:09:31 +0100

    Discover Header

--- pages/discover/index.tsx
+++ pages/discover/index.tsx
@@ -4,11 +4,21 @@ import styles from "./Discover.module.css";
 
 const Discover = () => {
   return (
-    <div className={styles.discover}>
+    <main className={styles.discover}>
       <header>
         <Searchbar />
+        <blockquote className={styles.blockQuote}>
+          <p>
+            Discover the perfect image for your design
+            <span className={styles.dot}></span>
+          </p>
+          <cite>
+            <span>&mdash;&mdash;&mdash; Photographed by:</span>{" "}
+            <span>Maarten bouwkamp</span>
+          </cite>
+        </blockquote>
       </header>
-    </div>
+    </main>
   );
 };
 
