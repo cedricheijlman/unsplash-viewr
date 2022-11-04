c64cca9ab66ec50b2b7647f2d90e8181b4079589
Cedric Heijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-11-04 08:43:13 +0100

    Navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Navbar.module.css";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 const Navbar = () => {
+  const router = useRouter();
+
+  const [routePath, setRoutePath] = useState(router.asPath);
+
   return (
     <nav className={styles.navBar}>
       <ul className={styles.navBarList}>
-        <li>About</li>
-        <li>Discover</li>
+        <li className={routePath == "/about" ? styles.active : ""}>
+          <Link href="/about">About</Link>
+        </li>
+        <li className={routePath == "/discover" ? styles.active : ""}>
+          <Link href="/discover">Discover</Link>
+        </li>
         <li id={styles.logo}>Viewr</li>
-        <li>Users</li>
-        <li>Account</li>
+        <li className={routePath == "/users" ? styles.active : ""}>
+          <Link href="/users">Users</Link>
+        </li>
+        <li className={routePath == "/account" ? styles.active : ""}>
+          <Link href="/account">Account</Link>
+        </li>
       </ul>
     </nav>
   );
